b7358d9e526c0ef0b579576da83cba7391d86346
hunk <hunkhuang@yizi.com.cn>
2015-07-30 18:02:35 +0800

    learn servlet.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 学习JavaEE
 
 day01.xml基础
+
+day02.servlet
---
+++ src/com/hunk/learn/servlet/RequestDemo2.java
@@ -0,0 +1,46 @@
+package com.hunk.learn.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+/**
+ * 注意：tomcat服务器首先会调用servlet的service方法，然后在service方法中再根据请求方式来分别调用对应的doXX方法
+ * （例如，如果是GET请求方式，在service方法中调用doGet方法）
+ * 
+ *   因为最常的请求方式是GET 和POST，所以编写servlet程序，只需要覆盖doGet和doPost即可！！！！
+ */
+/**
+ * Servlet implementation class RequestDemo2
+ */
+@WebServlet("/RequestDemo2")
+public class RequestDemo2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RequestDemo2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("GET方式提交");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Post方式提交");
+	}
+
+}
