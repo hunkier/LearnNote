8c9c3c998308f405a6bdb874af7dfee9ceed8123
huangkui <huangkuier@gmail.com>
2018-05-09 17:45:35 +0800

    juc cas
    Signed-off-by: huangkui <huangkuier@gmail.com>

---
+++ java/juc/src/main/java/com/hunk/juc/TestABCAlternate.java
@@ -0,0 +1,12 @@
+package com.hunk.juc;
+
+/**
+ * 编写一个程序，开启3个线程，这三个线程分别为A、B、C， 每个线程将自己的 ID 在屏幕上打印10遍，要求结果必须按顺序显示
+ */
+public class TestABCAlternate {
+
+}
+
+class AlternateDemo{
+
+}
---
+++ java/juc/src/main/java/com/hunk/juc/TestCompareAndSwap.java
@@ -0,0 +1,63 @@
+package com.hunk.juc;
+
+/**
+ * 模拟 CAS 算法
+ */
+public class TestCompareAndSwap {
+
+
+    public static void main(String[] args) {
+        final  CompareAnsSwap cas = new CompareAnsSwap();
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    int expectedValue = cas.get();
+                    boolean b = cas.compareAndSet(expectedValue, (int) (Math.random() * 2000));
+                    System.out.println(b);
+                }
+            }).start();
+        }
+    }
+}
+
+
+class CompareAnsSwap{
+
+
+    private int value;
+
+    /**
+     * 获取内存值
+     * @return
+     */
+    public synchronized int get(){
+        return value;
+    }
+
+    /**
+     * 比较
+     * @param expectedValue
+     * @param newValue
+     * @return
+     */
+    public synchronized int compareAndSwap(int expectedValue, int newValue){
+        int oldValue = value ;
+        if (oldValue == expectedValue){
+            this.value = newValue;
+        }
+
+        return oldValue;
+    }
+
+    /**
+     * 设置
+     * @param expectedValue
+     * @param newValue
+     * @return
+     */
+    public synchronized boolean compareAndSet(int expectedValue, int newValue){
+        return expectedValue == compareAndSwap(expectedValue,newValue);
+
+    }
+}
