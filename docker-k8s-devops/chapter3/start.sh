0c279ddf16275fdcd772a591f63e047dfac5cd04
huangkui <huangkuier@gmail.com>
2018-08-26 22:34:13 +0800

    dockerfile
    Signed-off-by: huangkui <huangkuier@gmail.com>

--- docker-k8s-devops/chapter3/start.sh
+++ docker-k8s-devops/chapter3/start.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+sudo yum install  -y  docker-ce
 sudo mkdir -p /etc/docker
 sudo tee /etc/docker/daemon.json <<-'EOF'
 {
@@ -15,4 +18,17 @@ sudo tee /etc/docker/daemon.json <<-'EOF'
 }
 
 EOF
-
+sudo gpasswd -a vagrant docker
+sudo yum install -y git vim gcc glibc-static telnet bridge-utils net-tools
+sudo systemctl start docker
+sudo docker stop redis
+sudo docker rm redis
+sudo docker run -idt -p 6379:6379 --name redis -v /home/vagrant/soft/redis/conf/redis.conf:/usr/local/etc/redis/redis.conf redis
+sudo docker stop elasticsearch
+sudo docker rm elasticsearch
+sudo docker run -idt -p 9200:9200 --name elasticsearch -v /home/vagrant/soft/elasticsearch/data/:/usr/share/elasticsearch/data -e "http.host=0.0.0.0" -e "transport.host=127.0.0.1" -e xpack.security.enabled=false elasticsearch
+sudo docker stop mysql
+sudo docker rm mysql
+sudo docker run -idt -p 3306:3306 --name mysql  -v /home/vagrant/soft/mysql/datadir:/var/lib/mysql -v /home/vagrant/soft/mysql/conf.d:/etc/mysql/conf.d  -e MYSQL_ROOT_PASSWORD=123456 -d mysql:5.6
+sudo docker run centos
+sudo docker run hello-world
