3be2974e07ea4c10ba60393caafb592528443cf3
huangkui <huangkuier@gmail.com>
2018-11-02 22:34:58 +0800

    groovy

--- gradle/GroovySpecification/src/variable/clouserstudy.groovy
+++ gradle/GroovySpecification/src/variable/clouserstudy.groovy
@@ -47,4 +47,42 @@ int z = cal(101)
 //println z
 
 // 字符串与闭包的结合使用
+String str = 'the 2 and 3 is 5'
+// each的遍历
+println  str.each {
+//    String temp -> print temp.multiply(2)
+}
 
+// find来查找符合条件打的第一个
+def num = str.find {
+    String s -> s.isNumber()
+}
+//println num
+
+def list = str.findAll {
+    String s -> s.isNumber()
+}
+//println list.toListString()
+
+def any = str.any {
+    String s -> s.isNumber()
+}
+//println any
+
+def every = str.every {
+    String s -> s.isNumber()
+}
+//println every
+
+def list2 = str.collect { it.toUpperCase()}
+//println list2
+
+/**
+ * 闭包的三个重要变量：this， owner，delegate
+ */
+def scriptClouser ={
+    println "scriptClouse this:" + this // 代表闭包定义处的类
+    println "scriptClouse owner:" + owner // 代表闭包定义处的类或者对象
+    println "scriptClouse delegate:" + delegate // 代表任意对象，默认与owner一致
+}
+scriptClouser.call()
--- netty/ssy/netty_lecture/note.md
+++ netty/ssy/netty_lecture/note.md
@@ -3,3 +3,10 @@ netty官网：https://netty.io/
 Gradle官网：https://gradle.org/
 
 使用gradle管理编译项目
+
+查看端口占用
+
+```shell
+lsof -i:8899
+```
+
