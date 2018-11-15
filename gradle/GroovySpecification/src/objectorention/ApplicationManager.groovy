1df46f3574bdd9ea817a168295df5ec3e3170d65
huangkui <huangkuier@gmail.com>
2018-11-15 09:21:59 +0800

    groovy object
    Signed-off-by: huangkui <huangkui@revenco.com>

---
+++ gradle/GroovySpecification/src/objectorention/ApplicationManager.groovy
@@ -0,0 +1,15 @@
+package objectorention
+
+/**
+ * 模拟一个应用的管理类
+ */
+class ApplicationManager {
+    static void init(){
+        ExpandoMetaClass.enableGlobally()
+        // 为第三方类添加方法
+        Person.metaClass.static.createPerson = {
+            String name, int age ->
+                new Person(name:name,age:age)
+        }
+    }
+}
