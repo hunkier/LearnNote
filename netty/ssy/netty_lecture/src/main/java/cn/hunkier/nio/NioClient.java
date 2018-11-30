13bf98bcf05b5790db7880fc477b2294e0c2f8d4
huangkui <huangkuier@gmail.com>
2018-11-30 23:03:28 +0800

    java.nio charset
    Signed-off-by: huangkui <huangkuier@gmail.com>

--- netty/ssy/netty_lecture/src/main/java/cn/hunkier/nio/NioClient.java
+++ netty/ssy/netty_lecture/src/main/java/cn/hunkier/nio/NioClient.java
@@ -2,11 +2,17 @@ package cn.hunkier.nio;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
+import java.time.LocalDateTime;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 public class NioClient {
@@ -26,10 +32,48 @@ public class NioClient {
                 if (selectionKey.isConnectable()){
                     SocketChannel client = (SocketChannel) selectionKey.channel();
                     if (client.isConnectionPending()){
+                        client.finishConnect();
+                        ByteBuffer writeBufer = ByteBuffer.allocate(1024);
+                        writeBufer.put((LocalDateTime.now() + " 连接成功").getBytes());
+                        writeBufer.flip();
+                        client.write(writeBufer);
 
+                        ExecutorService executorService = Executors.newSingleThreadExecutor();
+                        executorService.submit(()->{
+                            while (true){
+                                try {
+                                    writeBufer.clear();
+                                    InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+                                    BufferedReader br = new BufferedReader(inputStreamReader);
+
+                                    String sendMessage = br.readLine();
+
+                                    writeBufer.put(sendMessage.getBytes());
+                                    writeBufer.flip();
+                                    client.write(writeBufer);
+
+                                }catch (Exception e){
+                                    log.error("client error", e);
+                                }
+                            }
+                        });
+                    }
+
+                    client.register(selector, SelectionKey.OP_READ);
+                }else if(selectionKey.isReadable()){
+                    SocketChannel client = (SocketChannel) selectionKey.channel();
+
+                    ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+
+                    int count = client.read(readBuffer);
+
+                    if (count > 0 ){
+                        String receivedMessage = new String(readBuffer.array());
+                        log.info(receivedMessage);
                     }
                 }
             }
+            selectionKeys.clear();
         }
     }
 }
---
+++ netty/ssy/netty_lecture/src/main/java/cn/hunkier/nio/NioTest13.java
@@ -0,0 +1,46 @@
+package cn.hunkier.nio;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+
+@Slf4j
+public class NioTest13 {
+    public static void main(String[] args) throws Exception{
+        String inputFile = "settings.gradle";
+        String outputFile = "out/NioTest13_out.txt";
+        File file = new File(outputFile);
+        if (!file.getParentFile().exists()){
+            file.getParentFile().mkdirs();
+        }
+        file.createNewFile();
+
+        RandomAccessFile inputRandomAccessFile = new RandomAccessFile(inputFile, "r");
+        RandomAccessFile outputRandomAccessFile = new RandomAccessFile(outputFile, "rw");
+
+        long inputLength = new File(inputFile).length();
+
+        FileChannel inputFileChannel = inputRandomAccessFile.getChannel();
+        FileChannel outputFileChannel = outputRandomAccessFile.getChannel();
+        MappedByteBuffer inputData = inputFileChannel.map(FileChannel.MapMode.READ_ONLY, 0, inputLength);
+
+        Charset charset = Charset.forName("utf-8");
+        CharsetDecoder decoder = charset.newDecoder();
+        CharsetEncoder encoder = charset.newEncoder();
+
+        CharBuffer charBuffer = decoder.decode(inputData);
+        ByteBuffer outputData = encoder.encode(charBuffer);
+        outputFileChannel.write(outputData);
+        inputRandomAccessFile.close();
+        outputRandomAccessFile.close();
+
+    }
+}
