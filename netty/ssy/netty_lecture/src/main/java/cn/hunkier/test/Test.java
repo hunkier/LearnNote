b444840845be638d2de3a7ee623e267e9d60dd47
huangkui <huangkuier@gmail.com>
2018-12-13 22:49:20 +0800

    netty NioEventLoopGroup
    Signed-off-by: huangkui <huangkuier@gmail.com>

--- netty/ssy/netty_lecture/src/main/java/cn/hunkier/test/Test.java
+++ netty/ssy/netty_lecture/src/main/java/cn/hunkier/test/Test.java
@@ -5,8 +5,6 @@ import io.netty.util.internal.SystemPropertyUtil;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-
-
 public class Test {
     public static void main(String[] args) {
         // netty NioEventLoopGroup 默认线程数，为cpu超线程数*2
