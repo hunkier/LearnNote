ef8969bccd7da7f4c7535af66064190c6b03d6fe
hunkier <huangkuier@gmail.com>
2021-12-25 00:00:56 +0800

    python mod

--- PythonDemo/class_test.py
+++ PythonDemo/class_test.py
@@ -10,13 +10,13 @@ print_role(user1)
 
 
 class Player():     # 定义一个函数
-    def __init__(self, name, hp, occ):
+    def __init__(self, name, hp, occu):
         self.__name = name # 下划线开头的属性只能通过方法访问和修改
         self.hp = hp
-        self.occ = occ
+        self.occu = occu
 
     def print_role(self):
-        print('%s: %s  %s' % (self.__name, self.hp, self.occ))
+        print('%s: %s  %s' % (self.__name, self.hp, self.occu))
 
     def update_name(self,new_name):
         self.__name = new_name
@@ -24,7 +24,16 @@ class Player():     # 定义一个函数
 
 class Monster():
     '定义怪物类'
+    def __init__(self,hp):
+        self.hp = hp
+class Animals(Monster):
+    '普通怪物'
     pass
+class Boss(Monster):
+    'Boss 类怪物'
+    pass
+
+
 
 
 user1 = Player('tom', 100, 'war') # 类实例化
